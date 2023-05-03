88cbedb487c968f0cd9d322211edd6dda7cbe8fc
Tran Cao Bao Phuc <62349601+CrazyMeowl@users.noreply.github.com>
2023-05-03 22:02:32 +0700

    Added Router Dom

--- de_fronte/src/App.js
+++ de_fronte/src/App.js
@@ -1,29 +1,42 @@
 import logo from './logo.svg';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // import from components
 import DeNavBar  from './components/DeNavBar';
-
+import { Route, Routes } from 'react-router-dom';
+import {Home} from "./pages/Home"
+import {Cars} from "./pages/Cars"
+import {NotFound } from './pages/NotFound';
 function App() {
   return (
-    <div className="App">
-      <DeNavBar></DeNavBar>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <DeNavBar></DeNavBar>
+    //   {/* <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header> */}
+    // </div>
+    <>
+    <DeNavBar>
+
+    </DeNavBar>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/Cars" element={<Cars />}/>
+      <Route path="*" element={<NotFound />}/>
+    </Routes>
+    </>
+    
   );
 }
 
--- de_fronte/src/components/DeNavBar.js
+++ de_fronte/src/components/DeNavBar.js
@@ -2,24 +2,24 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-
+import {NavLink } from 'react-router-dom';
 function DeNavBar() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-          <Navbar.Brand href="#home">
+          <Navbar.Brand href="/">
             <img
-              src="https://is.sankakucomplex.com/data/a6/ef/a6ef8b10c20a612783ac8e89dfef8a88.jpg?e=1681744514&m=tzKpCfC2Nn7i2xy_kFiRlA"
+              src="######"
               width="30"
               height="30"
               className="d-inline-block align-top"
               alt="React Bootstrap logo"
-            />NAVBAR TEXT</Navbar.Brand>
+            /> NAVBAR TEXT</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <NavLink to="/"> Home </NavLink> 
+            <NavLink to="/cars"> Cars </NavLink>
             <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
