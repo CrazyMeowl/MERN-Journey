7dfa5c1e4691717567c94edc109e1a03947c80cd
Tran Cao Bao Phuc <62349601+CrazyMeowl@users.noreply.github.com>
2023-07-21 06:57:22 +0700

    Temp Commit
    Added some modules

--- le_bacon/app.js
+++ le_bacon/app.js
@@ -1,7 +1,21 @@
+require('dotenv').config()
+
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 const app = express()
 const port = 5000
+
+mongoose.connect(process.env.DATABASE_URL)
+
+const db = mongoose.connection
+
+db.on('error',(error)=> console.error(error))
+db.once('open',()=> console.log('Connected to database'))
+
+app.use(express.json())
+
+const mernRouter = require('./routes/mern')
 
 app.use(morgan('combined'))
 
